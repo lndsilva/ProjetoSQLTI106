ee99ac52ebe94ca1dec3dcda7ac88f3968d763c4
lndsilva <lndsilva@hotmail.com>
2022-11-17 15:05:09 -0300

    ligando tabelas por chave estrangeira

--- dbEmpresa.sql
+++ dbEmpresa.sql
@@ -24,6 +24,15 @@ primary key(codusu),
 foreign key(codfunc)references tbFuncionarios(codfunc)
 );
 
+create table tbFornecedores(
+codForn int not null auto_increment,
+nome varchar(100),
+cnpj char(18),
+inscrEst char(15),
+email varchar(100),
+telefone char(10),
+primary key(codForn)
+);
 
 create table tbProdutos(
 codProd int not null auto_increment,
@@ -31,13 +40,17 @@ descricao varchar(100),
 quantidade int default 0 check(quantidade >= 0),
 valor decimal(9,2) check(valor >= 0),
 codusu int not null,
+codForn int not null,
 primary key(codProd),
-foreign key(codusu)references tbUsuarios(codusu)
+foreign key(codusu)references tbUsuarios(codusu),
+foreign key(codForn)references tbFornecedores(codForn)
 );
+
 
 desc tbProdutos;
 desc tbUsuarios;
 desc tbFuncionarios;
+desc tbFornecedores;
 
 
 insert into tbFuncionarios(nome,email,cpf,salario,sexo)
@@ -46,10 +59,16 @@ values('Paulo Miranda da Silva','paulo.msilva@hotmail.com','258.247.248-00','150
 insert into tbUsuarios(nome,senha,codfunc)values('paulo.msilva','123456',1);
 
 
-insert into tbProdutos(descricao,quantidade,valor,codusu)values('Banana',12,10.35,1);
+insert into tbFornecedores(nome,cnpj,inscrEst,email,telefone)
+	values('Fabricantes S/A','25.147.235/0001-38','253.235.147-110',
+		'sac@empresa.com.br','97485-8574');
+
+
+insert into tbProdutos(descricao,quantidade,valor,codusu,codForn)
+	values('Banana',12,10.35,1,1);
 
 
 select * from tbFuncionarios;
 select * from tbUsuarios;
+select * from tbFornecedores;
 select * from tbProdutos;
-
